5d89af6e96d68ab0280febd17a13c11b1ac9ebdb
toyman640 <falakoadebayor@gmail.com>
2024-09-16 11:46:03 +0100

    Create Sign up form

--- src/component/SignUp.js
+++ src/component/SignUp.js
@@ -1,8 +1,47 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Row, Col } from 'react-bootstrap'; // Import Row and Col for grid layout
 
 const SignUp = () => (
   <div>
-    <h2>SignUp Page</h2>
+    <h2 className="text-center">SignUp Page</h2>
+    <Row className="justify-content-center">
+      <Col lg={{ span: 8, offset: 1 }} md={{ span: 10, offset: 1 }} sm={{ span: 10, offset: 1 }}>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter First Name" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="text" placeholder="Last Name" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicName">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="text" placeholder="Username" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control type="password" placeholder="Confirm Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="success" type="submit">
+            Register
+          </Button>
+        </Form>
+      </Col>
+    </Row>
   </div>
 );
 
