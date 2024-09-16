ed780cdc58e011cccc87f9b638b84c22b79ceb00
toyman640 <falakoadebayor@gmail.com>
2024-09-16 12:39:59 +0100

    Create sidebar and main component

--- src/component/dashboard/MainComp.js
+++ src/component/dashboard/MainComp.js
@@ -1,11 +1,20 @@
 import React from 'react';
+// import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import SideBar from './SideBar';
 import DashContent from './DashContent';
 
 const MainComp = () => (
   <div>
-    <SideBar />
-    <DashContent />
+    <Row>
+      <Col md={3} lg={2}>
+        <SideBar />
+      </Col>
+      <Col md={9} lg={10}>
+        <DashContent />
+      </Col>
+    </Row>
   </div>
 );
 
