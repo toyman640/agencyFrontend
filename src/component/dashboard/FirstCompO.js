14a498657a875723d179e3d2c2932f336efde3b4
toyman640 <falakoadebayor@gmail.com>
2024-09-17 16:08:10 +0100

    Add card to home page

--- src/component/dashboard/FirstCompO.js
+++ src/component/dashboard/FirstCompO.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faScrewdriverWrench } from '@fortawesome/free-solid-svg-icons';
+import CountUp from 'react-countup';
 
 const FirstCompO = () => (
   <div>
@@ -9,56 +12,60 @@ const FirstCompO = () => (
       <Col lg={3} md={6} sm={12}>
         <Card className="Card">
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the cards content.
+              <div className="CardInner">
+                <FontAwesomeIcon className="CardIcon" icon={faScrewdriverWrench} />
+                <p>
+                  <CountUp end={86} duration={2} start={0} separator="," />
+                </p>
+              </div>
             </Card.Text>
-            <Card.Link href="#">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
+            <Card.Subtitle className="mb-2 text-muted text-center">PROJECTS COMPLETED</Card.Subtitle>
           </Card.Body>
         </Card>
       </Col>
       <Col lg={3} md={6} sm={12}>
         <Card className="Card">
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the cards content.
+              <div className="CardInner">
+                <FontAwesomeIcon className="CardIcon" icon={faScrewdriverWrench} />
+                <p>
+                  <CountUp end={7} duration={2} start={0} separator="," />
+                </p>
+              </div>
             </Card.Text>
-            <Card.Link href="#">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
+            <Card.Subtitle className="mb-2 text-muted text-center">ONGOING COMPLETED</Card.Subtitle>
           </Card.Body>
         </Card>
       </Col>
       <Col lg={3} md={6} sm={12}>
         <Card className="Card">
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the cards content.
+              <div className="CardInner">
+                <FontAwesomeIcon className="CardIcon" icon={faScrewdriverWrench} />
+                <p>
+                  <CountUp end={7} duration={2} start={0} separator="," />
+                </p>
+              </div>
             </Card.Text>
-            <Card.Link href="#">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
+            <Card.Subtitle className="mb-2 text-muted text-center">LOCAL PROJECTS</Card.Subtitle>
           </Card.Body>
         </Card>
       </Col>
       <Col lg={3} md={6} sm={12}>
         <Card className="Card">
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the cards content.
+              <div className="CardInner">
+                <FontAwesomeIcon className="CardIcon" icon={faScrewdriverWrench} />
+                <p>
+                  <CountUp end={7} duration={2} start={0} separator="," />
+                </p>
+              </div>
             </Card.Text>
-            <Card.Link href="#">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
+            <Card.Subtitle className="mb-2 text-muted text-center">FOREGIN PROJECTS</Card.Subtitle>
           </Card.Body>
         </Card>
       </Col>
